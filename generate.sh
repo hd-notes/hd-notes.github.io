e1ea9205ff69b71bc5651d07e7ab3fe6e338d415
Robin Heinemann <robin.ole.heinemann@t-online.de>
2016-10-21 18:37:14 +0200

    add analytics

--- generate.sh
+++ generate.sh
@@ -4,6 +4,15 @@ DIR=${1:-../lectures}
 HEADER="<!DOCTYPE html>\n
 	<html>\n
 	<head>\n
+	<meta charset='utf-8'>\n
+	<script>\n
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');\n
+	ga('create', 'UA-86095188-1', 'auto');\n
+	ga('send', 'pageview');\n
+	</script>\n
 	</head>\n
 	<body>\n
 	<h1>Mitschrift Mathe / Physik</h1>\n"
