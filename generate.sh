90cd58c93d98f3f640351d431d1fbd3b89fe8a49
Robin Heinemann <robin.ole.heinemann@t-online.de>
2016-10-23 14:23:34 +0200

    update generate

--- generate.sh
+++ generate.sh
@@ -5,16 +5,15 @@ HEADER="<!DOCTYPE html>\n
 	<html>\n
 	<head>\n
 	<meta charset='utf-8'>\n
-	<script>\n
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');\n
-	ga('create', 'UA-86095188-1', 'auto');\n
-	ga('send', 'pageview');\n
-	</script>\n
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\n
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\n
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n
+	})(window,document,'script','dataLayer','GTM-M79WG8');</script>\n
 	</head>\n
 	<body>\n
+	<noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-M79WG8'\n
+	height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>\n
 	<h1>Mitschrift Mathe / Physik</h1>\n"
 
 FOOTER="</body>\n
